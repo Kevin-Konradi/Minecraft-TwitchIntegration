d8f6ea955226f826454b570a90d39972624c6c67
Kevin K <68509299+Kevin-Konradi@users.noreply.github.com>
2021-02-10 22:45:47 +0100

    Initial commit

---
+++ src/main/java/de/devkevtech/TwitchIntegration.java
@@ -0,0 +1,15 @@
+package de.devkevtech;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TwitchIntegration extends JavaPlugin {
+
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
